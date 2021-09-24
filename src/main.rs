dc67de5daa0b94c77eea72754675dd7a908ade71
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-09-24 15:09:49 +0200

    towards initializing rustls

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use clap::Clap;
 
 #[derive(Clap, Debug)]
+struct Args {
+    domains: Vec<String>,
+    #[clap(flatten)]
+    opt: Opt,
+}
+
+#[derive(Clap, Debug)]
 struct Opt {
     #[clap(short, long, default_value = "14")]
     days: u32,
-    domains: Vec<String>,
 }
 
 fn main() {
-    let opt = Opt::parse();
-    dbg!(opt);
+    let args = Args::parse();
+    for domain in args.domains {
+        validate(domain, &args.opt)
+    }
+}
+
+fn validate(domain: String, opt: &Opt) {
+    let mut config = rustls::ClientConfig::new();
+    config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+
+
 }
